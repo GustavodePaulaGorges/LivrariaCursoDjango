87de51c02d40f50fea54511dca3922077a26421d
Gustavo <gustavodep.gorges@gmail.com>
2022-10-07 16:39:41 -0300

    Primeira Versão

---
+++ core/views.py
@@ -0,0 +1,9 @@
+import imp
+from django.http import HttpResponse
+
+def teste(request):
+    return HttpResponse("Olá mundo do Django")
+
+def teste2(request):
+    return HttpResponse("Outra Página")
+
