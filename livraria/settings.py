79b19af128979c5e548da1032253fc5b5470df31
Gustavo <gustavodep.gorges@gmail.com>
2022-10-21 15:16:18 -0300

    correção de bug

--- livraria/settings.py
+++ livraria/settings.py
@@ -30,7 +30,8 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "drf_spectacular",
-    "corsheaders    " "core",
+    "corsheaders",
+    "core",
 ]
 
 MIDDLEWARE = [
