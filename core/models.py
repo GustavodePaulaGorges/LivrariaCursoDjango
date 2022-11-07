c92843610ed119ff0a50489a0408233ca2c6413b
Gustavo <gustavodep.gorges@gmail.com>
2022-11-07 15:27:53 -0300

    aperfeiçoei para o uso no projeto api

--- core/models.py
+++ core/models.py
@@ -45,27 +45,3 @@ class Livro(models.Model):
         return "%s (%s)" % (self.titulo, self.editora)
 
 
-class Compra(models.Model):
-    class StatusCompra(models.IntegerChoices):
-        CARRINHO = 1, "Carrinho"
-        REALIZADO = 2, "Realizado"
-        PAGO = 3, "Pago"
-        ENTREGUE = 4, "Entregue"
-
-    usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="compras")
-    status = models.IntegerField(
-        choices=StatusCompra.choices, default=StatusCompra.CARRINHO
-    )
-
-    @property
-    def total(self):
-        queryset = self.itens.all().aggregate(
-            total=models.Sum(F("quantidade") * F("livro__preco"))
-        )
-        return queryset["total"]
-
-
-class ItensCompra(models.Model):
-    compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="itens")
-    livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name="+")
-    quantidade = models.IntegerField()
